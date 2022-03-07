4e613d41f3aca408202b7f3adf242b943365a4ed
Alexandru Zaharia <alex.zaharia@shiftstudio.com>
2022-03-07 20:12:42 +0200

    Fix highlight on chart values
    The fix is provided by the community and applied based on the following pull request:
    https://github.com/danielgindi/Charts/pull/4721

--- Source/Charts/Data/Implementations/Standard/ChartDataSet.swift
+++ Source/Charts/Data/Implementations/Standard/ChartDataSet.swift
@@ -196,10 +196,10 @@ open class ChartDataSet: ChartBaseDataSet
     /// An empty array if no Entry object at that index.
     open override func entriesForXValue(_ xValue: Double) -> [ChartDataEntry]
     {
-        let match: (ChartDataEntry) -> Bool = { $0.x == xValue }
+        let match: (ChartDataEntry) -> Bool = { $0.x >= xValue }
         let i = partitioningIndex(where: match)
-        guard i < endIndex else { return [] }
-        return self[i...].prefix(while: match)
+        guard i < endIndex, self[i].x == xValue else { return [] }
+        return self[i...].prefix(while: { $0.x == xValue })
     }
     
     /// - Parameters:
@@ -234,7 +234,16 @@ open class ChartDataSet: ChartBaseDataSet
             }
 
         case .closest:
-            break
+            if closest > startIndex {
+                let closestXIndex = closest
+                formIndex(before: &closest)
+                let value = self[closest]
+
+                // If the x value is closer to the original x index revert closest otherwise fall through
+                if abs(value.x - xValue) > abs(closestXValue - xValue) {
+                    closest = closestXIndex
+                }
+            }
         }
 
         // Search by closest to y-value
